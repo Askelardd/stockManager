fc6a1b98e1d1178ddde00ce5a5cd5eb3a4f273de
askelardd <askelardd1@gmail.com>
2025-08-28 15:16:14 +0100

    login, more gitignore

--- stockManager/management/urls.py
+++ stockManager/management/urls.py
@@ -6,15 +6,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/<int:user_id>/', views.login, name='login'),
+    path('login/<int:user_id>/', views.login_view, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('listar_pos/', views.listar_pos, name='listar_pos'),
     path('po/<int:po_id>/adicionar/', views.adicionarMaisde1Po, name='adicionar_po'),
     path('po/<int:po_id>/remover/', views.removerPo, name='remover_po'),
     path('listar_update_pos/', views.listar_updates, name='listar_update_pos'),
-
     path('listar_fios/', views.listar_fios, name='listar_fios'),
     path('fio/<int:fio_id>/adicionar/', views.adicionarMaisde1Fio, name='adicionar_fio'),
     path('fio/<int:fio_id>/remover/', views.removerFio, name='remover_fio'),
     path('listar_update_fios/', views.listar_updates_fios, name='listar_update_fios'),
+
     
 ]
